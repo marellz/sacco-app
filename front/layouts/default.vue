b3776ef795fb77b3e0e7bff1bf57abf08d8c12be
Dave Njoroge <davenjorge10@gmail.com>
2024-12-28 20:33:00 +0300

    added home layout, redefined default for dashboard view

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="h-screen flex flex-col">
-        <LayoutsHeader/>
-        <main class="flex-auto">
-            <slot />
+        <main class="flex-auto flex p-4 gap-10">
+            <LayoutsSidebar />
+            <div class="flex-auto">
+                <slot />
+            </div>
         </main>
         <LayoutsFooter/>
     </div>
 </template>
 <script lang="ts" setup>
-
-
 </script>
---
+++ front/layouts/home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="h-screen flex flex-col">
+        <LayoutsHeader />
+        <main class="flex-auto">
+            <slot />
+        </main>
+        <LayoutsFooter />
+    </div>
+</template>
+<script lang="ts" setup>
+</script>
