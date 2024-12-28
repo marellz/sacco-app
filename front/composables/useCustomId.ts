279c1f18e98849f394a9b9e8f43d12bf44e53be0
Dave Njoroge <davenjorge10@gmail.com>
2024-12-28 19:05:40 +0300

    fix composable

--- front/composables/useCustomId.ts
+++ front/composables/useCustomId.ts
@@ -1,6 +1,5 @@
-const useCustomId = () => {
+const useCustomId = (length: number = 10) => {
   return [...Array(length)].map(() => Math.random().toString(36)[2]).join("");
 };
-
 
 export default useCustomId;
