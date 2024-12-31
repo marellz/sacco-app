4751f6ca5d749dae50d20617e6fe8d394cc01412
Dave Njoroge <davenjoroge10@gmail.com>
2024-12-31 10:26:30 +0300

    home component update

---
+++ front/components/home/header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header class="border-b py-2">
+        <LayoutsContainer class="flex items-center">
+            <nuxt-link to="/" class="block p-2 text-3xl font-bold">
+                WaweruSacco
+            </nuxt-link>
+            <div class="ml-5 flex space-x-3">
+                <span>Learn more</span>
+            </div>
+            <div class="ml-auto flex space-x-3">
+                <template v-if="auth.isAuthenticated">
+                   <UserMenu />
+                </template>
+                <template v-else>
+                    <nuxt-link to="/auth/login">
+                        <span>Login</span>
+                    </nuxt-link>
+                    <nuxt-link to="/auth/register">
+                        <span>Register</span>
+                    </nuxt-link>
+                </template>
+            </div>
+        </LayoutsContainer>
+    </header>
+</template>
+<script lang="ts" setup>
+import { useAuthStore } from '~/store/auth';
+
+const auth = useAuthStore();
+</script>
--- front/layouts/home.vue
+++ front/layouts/home.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="h-screen flex flex-col">
-        <LayoutsHeader />
+        <HomeHeader />
         <main class="flex-auto">
             <slot />
         </main>
-        <LayoutsFooter />
+        <HomeFooter />
     </div>
 </template>
 <script lang="ts" setup>
