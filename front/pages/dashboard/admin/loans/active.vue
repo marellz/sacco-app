6333fd006a6f93c80a9cceeb3689f06ce51ad741
Dave Njoroge <davenjoroge10@gmail.com>
2025-01-11 14:03:26 +0300

    sidebar and loan page structure update

---
+++ front/pages/dashboard/admin/loans/active.vue
@@ -0,0 +1,12 @@
+<template>
+    <LayoutsContainer>
+        <DashHeader title="Active loans"></DashHeader>
+        <DashContent>
+            {{ applications }}
+        </DashContent>
+    </LayoutsContainer>
+</template>
+<script lang="ts" setup>
+
+const applications = ref([])
+</script>
