8cdbd54184776f6e629fc2b308f60a459362b898
Dave Njoroge <davenjoroge10@gmail.com>
2025-01-11 21:25:31 +0300

    added loan status tag, added colors for that

---
+++ front/components/dash/loan/StatusTag.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="tag" :class="themes[variant]">
+        <slot />
+    </div>
+</template>
+<script lang="ts" setup>
+import type { LoanApplicationStatus } from '~/store/loans';
+
+defineProps<{
+    variant: LoanApplicationStatus
+}>()
+
+type Themes = {
+    [key in LoanApplicationStatus] : string
+}
+
+const themes : Themes = {
+    "draft": "bg-slate-100 text-slate-600",
+    "pending": "bg-saffron/10 text-saffron",
+    "approved": "bg-mantis/10 text-mantis",
+    "rejected": "bg-red-100 text-red-500",
+    "disbursed": "bg-russian-violet/10 text-russian-violet",
+    "repaid": "bg-viridian/10 text-viridian",
+    "withdrawn": "bg-chocolate-cosmos/20 text-chocolate-cosmos",
+}
+</script>
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -16,6 +16,22 @@ export default {
         "color-c": "#eb9486ff", // coral-pink
         "color-d": "#f3de8aff", // flax
         "color-e": "#f9f8f8ff", // seasalt
+
+        "": "",
+        "": "",
+        "": "",
+        "": "",
+        "": "",
+        "": "",
+
+        mantis: "#5fad56ff",
+        saffron: "#f2c14eff",
+        coral: "#f78154ff",
+        viridian: "#4d9078ff",
+        "raspberry-rose": "#b4436cff",
+        "light-sky-blue": "#85c7f2ff",
+        "russian-violet": "#20063bff",
+        "chocolate-cosmos": "#4e0110ff"
       },
     },
   },
